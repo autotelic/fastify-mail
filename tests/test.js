684cf62816a1665c520313999bd3078019276b6d
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-12-22 17:21:02 -0700

    Test createMessage()

--- tests/test.js
+++ tests/test.js
@@ -4,6 +4,9 @@ const fastifyMail = require('../')
 const pointOfView = require('point-of-view')
 const nodemailer = require('fastify-nodemailer')
 const ejs = require('ejs')
+const { resolve } = require('path')
+
+const testContext = { name: 'test-context' }
 
 test('fastify.mail exists', t => {
   t.plan(2)
@@ -43,6 +46,66 @@ test('fastify-mail throws error if plugin dependencies not registered:', t => {
 
     fastify.ready(err => {
       t.ok(err instanceof Error)
+    })
+  })
+  t.end()
+})
+
+test('fastify.mail.createMessage: ', t => {
+  t.test('gets context dynamically', t => {
+    t.plan(3)
+    const fastify = Fastify()
+
+    fastify.register(nodemailer)
+    fastify.register(pointOfView, {
+      engine: {
+        ejs
+      },
+      includeViewExtension: true,
+      options: {
+        filename: resolve('templates')
+      }
+    })
+    fastify.register(fastifyMail)
+
+    fastify.ready(async err => {
+      t.error(err)
+      const queued = await fastify.mail.createMessage(testContext)
+
+      t.ok(queued, 'message was created')
+      const { html } = queued
+      t.ok(html.includes(testContext.name), 'created html with context')
+
+      fastify.close()
+    })
+  })
+
+  t.test('renders html with includes', t => {
+    t.plan(4)
+    const fastify = Fastify()
+
+    fastify.register(nodemailer)
+    fastify.register(pointOfView, {
+      engine: {
+        ejs
+      },
+      includeViewExtension: true,
+      options: {
+        filename: resolve('templates')
+      }
+    })
+    fastify.register(fastifyMail)
+
+    fastify.ready(async err => {
+      t.error(err)
+      const queued = await fastify.mail.createMessage(testContext)
+
+      t.ok(queued, 'message was created')
+      const { html } = queued
+      t.ok(html.includes('<header>'), 'created html includes header')
+      t.ok(html.includes('<footer>'), 'created html includes footer')
+
+      fastify.close()
     })
   })
   t.end()
